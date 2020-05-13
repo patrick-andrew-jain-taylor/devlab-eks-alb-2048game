a598dc7d04103985ea0baadecd8ce38b5bdba685
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-05-13 14:10:35 -0400

    Adding CAPABILITY_IAM

--- c9_cft.tf
+++ c9_cft.tf
@@ -5,4 +5,5 @@ data "template_file" "this" {
 resource "aws_cloudformation_stack" "this" {
   name = "devlab-eks-alb-2048game"
   template_body = data.template_file.this.rendered
+  capabilities = ["CAPABILITY_IAM"]
 }
