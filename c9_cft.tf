35889f2e716ce6b14199b967f6176f6646cfd0e0
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-05-13 14:08:45 -0400

    Adding CFT.

---
+++ c9_cft.tf
@@ -0,0 +1,8 @@
+data "template_file" "this" {
+  template = file("c9_cft.yaml")
+}
+
+resource "aws_cloudformation_stack" "this" {
+  name = "devlab-eks-alb-2048game"
+  template_body = data.template_file.this.rendered
+}
